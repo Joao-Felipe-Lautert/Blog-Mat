5a80b2e75d78a8f8bc3ecf42c56cc3522c2718c4
JoãoFelipeLautert <joao.lautert@outlook.com>
2025-11-10 18:52:06 -0300

    Teorema de Pitágoras V1.0

--- js/Pitagoras.js
+++ js/Pitagoras.js
@@ -2,7 +2,7 @@
 document.addEventListener('DOMContentLoaded', function(menu) {
     
     // Seleciona todos os elementos que têm a classe 'menu-botao'
-    const menuButtons = document.querySelectorAll('.menu-botao');
+    const menuButtons = document.querySelectorAll('#retornar');
     
     // Para cada botão, adiciona um atraso para a animação aparecer
     menuButtons.forEach((button, index) => {
@@ -32,3 +32,15 @@ document.addEventListener('DOMContentLoaded', function(credito) {
 
 });
 
+calcPythagoras = () => {
+    let a = parseFloat(document.getElementById('cateto-a').value);
+    let b = parseFloat(document.getElementById('cateto-b').value);
+    let c = Math.sqrt(a * a + b * b);
+    document.getElementById('pythagoras-result').innerText = `A hipotenusa é: ${c.toFixed(2)}`;
+}
+
+clearPythagoras = () => {
+    document.getElementById('cateto-a').value = '';
+    document.getElementById('cateto-b').value = '';
+    document.getElementById('pythagoras-result').innerText = '';
+}
